7632a51dc9f3e3692312cb1b46452a438ca563bc
jesperpedersen <jesper.pedersen@redhat.com>
2022-04-20 07:35:54 -0400

    Next is 0.1.2

--- src/include/pgexporter_ext.h
+++ src/include/pgexporter_ext.h
@@ -33,7 +33,7 @@
 extern "C" {
 #endif
 
-#define VERSION "0.1.1"
+#define VERSION "0.1.2"
 
 #define PGEXPORTER_EXT_HOMEPAGE "https://pgexporter.github.io/"
 #define PGEXPORTER_EXT_ISSUES "https://github.com/pgexporter/pgexporter_ext/issues"
